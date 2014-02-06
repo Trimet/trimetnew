9367debc138c7b8464b504bb7b45dd711c67b955
alexander fedorov <elf607@ya.ru>
2014-02-06 17:28:14 +0600

    making christmass..... etc..

--- services/views.py
+++ services/views.py
@@ -1,1 +1,35 @@
-# Create your views here.
+#-*- coding:utf-8 -*-
+
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+
+def show_index(request):
+
+    if request.is_ajax():
+        template = loader.get_template('services/index_ajax.html')
+    else:
+        template = loader.get_template('services/index.html')
+
+    context = RequestContext(request, {
+        'cmenu': 'services',
+        'title': 'Услуги',
+        'smenu': 'metalwork',
+    })
+
+    return HttpResponse(template.render(context))
+
+def show_metalwork(request):
+
+    if request.is_ajax():
+        template = loader.get_template('services/metalwork_ajax.html')
+    else:
+        template = loader.get_template('services/index.html')
+
+    context = RequestContext(request, {
+        'cmenu': 'services',
+        'title': 'Услуги',
+        'smenu': 'metalwork',
+    })
+
+    return HttpResponse(template.render(context))
